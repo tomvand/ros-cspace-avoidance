3f632304177e47f282f38c7b47f4d489f8336bb8
Tom van Dijk <tomvand@users.noreply.github.com>
2018-10-17 16:36:43 +0200

    Finish first version of ros node
    Seems to work ok in simulation.
    To-do: how to treat NaN's? Should these be seen as obstacles instead of gaps?

--- src/cspace.cpp
+++ src/cspace.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
 
 #include <vector>
 #include <cmath>
@@ -8,7 +10,7 @@
 namespace
 {
   template <typename T>
-  T bound(const T& val, const T &min, const T& max) {
+  T bound(const T& val, const T& min, const T& max) {
     if(val < min) return min;
     if(val > max) return max;
     return val;
@@ -18,7 +20,8 @@ namespace
     int width;
     int height;
     int ndisp;
-    double f;
+    double f; // Focal length of downscaled(!) image
+    double f_disp; // Focal length for disparity calculation
     double B;
   };
 
@@ -33,14 +36,14 @@ namespace
       lut_y1(ip.height, std::vector<int>(ip.ndisp)),
       lut_y2(ip.height, std::vector<int>(ip.ndisp)),
       lut_dnew(ip.ndisp)
-      {
+    {
       // For LUT generation, refer to Matthies et al., 2014 "Stereo vision..."
       // x1, x2 LUT
       for(int x = 0; x < ip.width; ++x) {
         for(int d = 1; d < ip.ndisp; ++d) { // Caution: d = 0 unhandled!
           double cx = ip.width / 2.0;
           double u = x - cx;
-          double zw = ip.f * ip.B / d;
+          double zw = ip.f_disp * ip.B / d;
           double xw = u * zw / ip.f;
           double alpha = std::atan(zw / xw);
           double alpha1 = std::asin(rv / std::sqrt(zw * zw + xw * xw));
@@ -50,6 +53,7 @@ namespace
           double x2 = bound<double>(cx + ip.f * r2x / zw, 0, ip.width - 1);
           lut_x1[x][d] = static_cast<int>(x1);
           lut_x2[x][d] = static_cast<int>(x2);
+//          ROS_INFO("(%i,%i): x1 = %i, x2 = %i", x, d, lut_x1[x][d], lut_x2[x][d]);
         }
       }
       // y1, y2 LUT
@@ -57,7 +61,7 @@ namespace
         for(int d = 1; d < ip.ndisp; ++d) { // Caution: d = 0 unhandled!
           double cy = ip.height / 2.0;
           double v = y - cy;
-          double zw = ip.f * ip.B / d;
+          double zw = ip.f_disp * ip.B / d;
           double yw = v * zw / ip.f;
           double beta = std::atan(zw / yw);
           double beta1 = std::asin(rv / std::sqrt(zw * zw + yw * yw));
@@ -67,14 +71,15 @@ namespace
           double y2 = bound<double>(cy + ip.f * r4y / zw, 0, ip.height - 1);
           lut_y1[y][d] = static_cast<int>(y1);
           lut_y2[y][d] = static_cast<int>(y2);
+//          ROS_INFO("(%i,%i): y1 = %i, y2 = %i, zw = %f", y, d, lut_y1[y][d], lut_y2[y][d], zw);
         }
       }
       // dnew LUT
       for(int d = 1; d < ip.ndisp; ++d) { // Caution: d = 0 unhandled!
-        double zw = ip.f * ip.B / d;
+        double zw = ip.f_disp * ip.B / d;
         double znew = zw - rv;
         if(znew < NEAR_CLIP) znew = NEAR_CLIP;
-        lut_dnew[d] = std::ceil(ip.f * ip.B / znew);
+        lut_dnew[d] = std::ceil(ip.f_disp * ip.B / znew);
       }
     }
 
@@ -135,7 +140,7 @@ namespace
             int x1 = lut.x1(x, d);
             int x2 = lut.x2(x, d);
             for(int x_write = x1; x_write <= x2; ++x_write) {
-              if(d > temp(y, x_write)) {
+              if(!(d <= temp(y, x_write))) { // Negative test to handle NaNs!
                 temp(y, x_write) = d;
               }
             }
@@ -152,7 +157,7 @@ namespace
             int y1 = lut.y1(y, d);
             int y2 = lut.y2(y, d);
             for(int y_write = y1; y_write <= y2; ++y_write) {
-              if(dnew > cspace(y_write, x)) {
+              if(!(dnew <= cspace(y_write, x))) { // Negative test to handle NaNs!
                 cspace(y_write, x) = dnew;
               }
             }
@@ -164,17 +169,47 @@ namespace
     ImageParams ip;
     LUT lut;
   };
+
+  class CSpaceNode {
+  public:
+    CSpaceNode(ImageParams ip, double rv) :
+      ce(ip, rv),
+      it(nh)
+    {
+      sub = it.subscribe("/disp_map/image", 1, &CSpaceNode::image_callback, this);
+    }
+//  private:
+    void image_callback(const sensor_msgs::ImageConstPtr& msg) {
+      cv::Mat_<float> disp(msg->height, msg->width, (float*)(&(msg->data[0])));
+      cv::Mat_<float> cspace;
+
+      ce.expand(disp, cspace);
+
+      cv::Mat_<float> debug;
+      cv::vconcat(disp, cspace, debug);
+
+      cv::imshow("cspace", debug / 63.0);
+      cv::waitKey(1);
+    }
+
+    CSpaceExpander ce;
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it;
+    image_transport::Subscriber sub;
+    image_transport::Publisher pub;
+  };
 } // namespace
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "cspace");
-  ros::NodeHandle nh;
   ImageParams ip = {
       .width = 96,
       .height = 96,
       .ndisp = 64,
-      .f = 425,
+      .f = 425 / 6, // Note: get from param for now, as camera_info does not arrive before construction...
+      .f_disp = 425,
       .B = 0.20,
   };
-  CSpaceExpander ce(ip, 1.0);
+  CSpaceNode c(ip, 1.0);
+  ros::spin();
 }
